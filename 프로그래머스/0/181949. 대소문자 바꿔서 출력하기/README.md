c6fc2c8e7393baab37d698dabc2f06b48e25fdac
GRIT <hinote444@naver.com>
2024-10-15 13:10:17 +0900

    [level 0] Title: 대소문자 바꿔서 출력하기, Time: 191.13 ms, Memory: 64.5 MB -BaekjoonHub

--- 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/README.md
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 대소문자 바꿔서 출력하기 - 181949 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181949) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181949?language=java) 
 
 ### 성능 요약
 
-메모리: 65.1 MB, 시간: 157.27 ms
+메모리: 64.5 MB, 시간: 191.13 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 10월 15일 12:44:49
+2024년 10월 15일 13:10:14
 
 ### 문제 설명
 
--- 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.java
+++ 프로그래머스/0/181949. 대소문자 바꿔서 출력하기/대소문자 바꿔서 출력하기.java
@@ -4,20 +4,19 @@ public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
-        StringBuilder b = new StringBuilder();
+        StringBuilder strb = new StringBuilder();
         
-        for(int i=0; i<str.length(); i++){
-            char a = str.charAt(i);
+        for(char ch : str.toCharArray()){
             
-            if (Character.isUpperCase(a)) {
-                b.append(Character.toLowerCase(a));
+            if (Character.isUpperCase(ch)) {
+                strb.append(Character.toLowerCase(ch));
             } else {
-                b.append(Character.toUpperCase(a));
+                strb.append(Character.toUpperCase(ch));
             }
         }
         
-        String c = b.toString();
-        System.out.println(c);
+        String result = strb.toString();
+        System.out.println(result);
         
         sc.close();
     }
