802ef6dbcc225b4cf39436d54cb20bff61b29a36
GRIT <hinote444@naver.com>
2024-12-12 06:47:03 +0900

    [level 0] Title: 원소들의 곱과 합, Time: 3.25 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/0/181929. 원소들의 곱과 합/README.md
+++ 프로그래머스/0/181929. 원소들의 곱과 합/README.md
@@ -1,10 +1,10 @@
 # [level 0] 원소들의 곱과 합 - 181929 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181929) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181929?language=java) 
 
 ### 성능 요약
 
-메모리: 69.9 MB, 시간: 0.02 ms
+메모리: 84.6 MB, 시간: 3.25 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 12일 06:40:56
+2024년 12월 12일 06:47:02
 
 ### 문제 설명
 
--- 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
+++ 프로그래머스/0/181929. 원소들의 곱과 합/원소들의 곱과 합.java
@@ -1,14 +1,11 @@
+import java.util.Arrays;
 class Solution {
     public int solution(int[] num_list) {
-        int product = 1;
-        int sum = 0;
-        for (int num : num_list) {
-            product *= num;
-            sum += num;
-        }
-        if (product < sum * sum) {
-            return 1;
-        }
-        return 0;
+        int product = Arrays.stream(num_list)
+                            .reduce(1, (a, b) -> a * b);
+        int sum = Arrays.stream(num_list)
+                        .sum();
+
+        return product < sum * sum ? 1 : 0;
     }
 }
