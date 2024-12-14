e6ce4333217efc1888d6d78df5d0e512f13946eb
GRIT <hinote444@naver.com>
2024-12-14 10:28:35 +0900

    [level 0] Title: 배열 만들기 4, Time: 20.29 ms, Memory: 98.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -0,0 +1,25 @@
+import java.util.*;
+class Solution {
+public int[] solution(int[] arr) {
+        List<Integer> stk = new ArrayList<>();
+        int i = 0;
+
+        while (i < arr.length) {
+            if (stk.isEmpty()) {
+                stk.add(arr[i]);
+                i++;
+                continue;
+            }
+            if (stk.get(stk.size() - 1) < arr[i]) {
+                stk.add(arr[i]);
+                i++;
+                continue;
+            }
+            stk.remove(stk.size() - 1);
+        }
+
+        return stk.stream()
+                     .mapToInt(Integer::intValue)
+                     .toArray();
+    }
+}
