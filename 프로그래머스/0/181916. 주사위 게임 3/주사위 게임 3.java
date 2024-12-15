a0f2e1e4937c5c233e25d0c9e89a1db61508d03b
GRIT <hinote444@naver.com>
2024-12-16 04:09:57 +0900

    [level 0] Title: 주사위 게임 3, Time: 1.20 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -2,39 +2,21 @@ import java.util.*;
 class Solution {
     public int solution(int a, int b, int c, int d) {
         int[] dices = {a, b, c, d};
-        Map<Integer, Integer> count = new HashMap<>();
-        for (int dice : dices) {
-            count.put(dice, count.getOrDefault(dice, 0) + 1);
-        }
+        Arrays.sort(dices);
 
-        if (count.size() == 1) {
-            return 1111 * a;
-        }
-        if (count.size() == 2) {
-            List<Integer> keys = new ArrayList<>(count.keySet());
-            int p = keys.get(0);
-            int q = keys.get(1);
+        a = dices[0];
+        b = dices[1];
+        c = dices[2];
+        d = dices[3];
 
-            if (count.get(p) == 3) {
-                return (int) Math.pow(10 * p + q, 2);
-            }
-            if (count.get(p) == 1) {
-                return (int) Math.pow(10 * q + p, 2);
-            }
-            if (count.get(p) == 2) {
-                return (p + q) * Math.abs(p - q);
-            }
-        }
-        if (count.size() == 3) {
-            List<Integer> keys = new ArrayList<>();
-            for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
-                if (entry.getValue() == 2) {
-                    continue;
-                }
-                keys.add(entry.getKey());
-            }
-            return keys.get(0) * keys.get(1);
-        }
-        return Math.min(Math.min(a, b), Math.min(c, d));
+        return
+        a == d ? 1111 * a :
+        a == c ? (10 * c + d) * (10 * c + d) :
+        b == d ? (10 * b + a) * (10 * b + a) :
+        a == b && c == d ? (c + b) * (c - b) :
+        a == b ? c * d :
+        b == c ? a * d :
+        c == d ? a * b :
+        a;
     }
 }
