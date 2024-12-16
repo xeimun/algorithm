e09f33f9277c6a87ecd33eec10d3e236d26a75d0
GRIT <hinote444@naver.com>
2024-12-17 02:28:53 +0900

    [level 0] Title: 접미사 배열, Time: 0.55 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -2,13 +2,9 @@ import java.util.*;
 class Solution {
     public String[] solution(String my_string) {
         String[] answer = new String[my_string.length()];
-        StringBuilder str = new StringBuilder(my_string);
-        answer[0] = my_string;
-        int len = my_string.length();
 
-        for (int i = 1; i < len; i++) {
-            str.deleteCharAt(0);
-            answer[i] = str.toString();
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = my_string.substring(i, my_string.length());
         }
 
         Arrays.sort(answer);
