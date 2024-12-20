3dadbfc087031bda1ec2120714304f9fa3a52a55
GRIT <hinote444@naver.com>
2024-12-20 11:07:16 +0900

    [level 0] Title: 순서 바꾸기, Time: 5.51 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -1,15 +1,8 @@
+import java.util.stream.*;
 class Solution {
     public int[] solution(int[] num_list, int n) {
-        int[] answer = new int[num_list.length];
-        int index = 0;
-        
-        for (int i = n; i < num_list.length; i++) {
-            answer[index++] = num_list[i];
-        }
-        for (int i = 0; i < n; i++) {
-            answer[index++] = num_list[i];
-        }
-        
-        return answer;
+        return IntStream.range(0, num_list.length)
+                        .map(i -> num_list[(i + n) % num_list.length])
+                        .toArray();
     }
 }
