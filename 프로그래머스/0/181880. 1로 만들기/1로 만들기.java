b9c572fb575124528cf3e05414db14b5f9b8107c
GRIT <hinote444@naver.com>
2024-12-22 19:05:55 +0900

    [level 0] Title: 1로 만들기, Time: 3.21 ms, Memory: 87.5 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
@@ -1,16 +1,6 @@
+import java.util.*;
 class Solution {
     public int solution(int[] num_list) {
-        int answer = 0;
-        for (int num : num_list) {
-            while (num > 1) {
-                if (num % 2 == 0) {
-                    num = num / 2;
-                } else {
-                    num = (num - 1) / 2;
-                }
-                answer++;
-            }
-        }
-        return answer;
+        return Arrays.stream(num_list).map(i -> Integer.toBinaryString(i).length() - 1).sum();
     }
 }
