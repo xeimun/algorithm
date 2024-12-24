97697e362524a3537a638de15303a8832f0153cd
GRIT <hinote444@naver.com>
2024-12-24 13:01:33 +0900

    [level 0] Title: ad 제거하기, Time: 3.44 ms, Memory: 88.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Solution {
+    public String[] solution(String[] strArr) {
+        return Arrays.stream(strArr)
+                     .filter(str -> !str.contains("ad"))
+                     .toArray(String[]::new);
+    }
+}
