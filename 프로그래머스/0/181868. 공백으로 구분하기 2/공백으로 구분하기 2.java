44537b31ee59d8c9e99625fdb46f16e839852a88
GRIT <hinote444@naver.com>
2024-12-24 14:38:57 +0900

    [level 0] Title: 공백으로 구분하기 2, Time: 1.17 ms, Memory: 81.1 MB -BaekjoonHub

--- 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
+++ 프로그래머스/0/181868. 공백으로 구분하기 2/공백으로 구분하기 2.java
@@ -1,9 +1,6 @@
 import java.util.*;
 class Solution {
    public String[] solution(String my_string) {
-        String[] arr = my_string.split(" ");
-        return Arrays.stream(arr)
-                     .filter(s -> !s.isBlank())
-                     .toArray(String[]::new);
+        return my_string.strip().split("\\s+");
     }
 }
