c6b8655a8934a7bcd846342fcadbcc18342f178c
GRIT <hinote444@naver.com>
2024-12-27 01:21:43 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 11.46 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.java
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.java
@@ -1,18 +1,12 @@
-import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import java.util.*;
+import java.util.stream.*;
 class Solution {
    public int[] solution(int[] arr, int k) {
-        List<Integer> tmp = Arrays.stream(arr)
-                                  .distinct()
-                                  .boxed()
-                                  .collect(Collectors.toList());
-        while (tmp.size() < k) {
-            tmp.add(-1);
-        }
-        return IntStream.range(0, k)
-                        .map(i -> tmp.get(i))
+        return IntStream.concat(Arrays.stream(arr)
+                                      .distinct(), IntStream.range(0, k)
+                                                            .map(i -> -1))
+                        .limit(k)
                         .toArray();
+
     }
 }
