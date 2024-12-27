b31a7922d86af81f3107e1cb900ba4a4daf36cad
GRIT <hinote444@naver.com>
2024-12-27 22:57:36 +0900

    [level 0] Title: 배열 비교하기, Time: 0.99 ms, Memory: 78.3 MB -BaekjoonHub

--- 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
@@ -1,24 +1,15 @@
 import java.util.*;
 class Solution {
     public int solution(int[] arr1, int[] arr2) {
-        if (arr1.length != arr2.length) {
-            if (arr1.length > arr2.length) {
-                return 1;
-            } else {
-                return -1;
-            }
-        } else {
-            int arr1Sum = Arrays.stream(arr1)
-                                .sum();
-            int arr2Sum = Arrays.stream(arr2)
-                                .sum();
-            if (arr1Sum > arr2Sum) {
-                return 1;
-            } else if (arr1Sum < arr2Sum) {
-                return -1;
-            } else {
-                return 0;
-            }
+        int answer = Integer.compare(arr1.length, arr2.length);
+        if (answer == 0) {
+            answer = Integer.compare(
+                    Arrays.stream(arr1)
+                          .sum(),
+                    Arrays.stream(arr2)
+                          .sum()
+            );
         }
+        return answer;
     }
 }
