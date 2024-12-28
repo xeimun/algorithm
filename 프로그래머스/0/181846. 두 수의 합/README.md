cbcaf5bdbca83dd723def3d01f90f455d7e50b04
GRIT <hinote444@naver.com>
2024-12-29 03:20:10 +0900

    [level 0] Title: 두 수의 합, Time: 14.51 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/0/181846. 두 수의 합/README.md
+++ 프로그래머스/0/181846. 두 수의 합/README.md
@@ -1,10 +1,10 @@
 # [level 0] 두 수의 합 - 181846 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181846) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181846?language=java) 
 
 ### 성능 요약
 
-메모리: 124 MB, 시간: 435.06 ms
+메모리: 75.7 MB, 시간: 14.51 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 29일 03:09:43
+2024년 12월 29일 03:20:08
 
 ### 문제 설명
 
--- 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
+++ 프로그래머스/0/181846. 두 수의 합/두 수의 합.java
@@ -1,8 +1,24 @@
 import java.math.*;
 class Solution {
     public String solution(String a, String b) {
-        BigDecimal c = new BigDecimal(a);
-        BigDecimal d = new BigDecimal(b);
-        return String.valueOf(c.add(d));
+        StringBuilder result = new StringBuilder();
+        int carry = 0;
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+
+        while (i >= 0 || j >= 0 || carry > 0) {
+            int sum = carry;
+            if (i >= 0) {
+                sum += a.charAt(i--) - '0';
+            }
+            if (j >= 0) {
+                sum += b.charAt(j--) - '0';
+            }
+            result.append(sum % 10);
+            carry = sum / 10;
+        }
+
+        return result.reverse()
+                     .toString();
     }
 }
