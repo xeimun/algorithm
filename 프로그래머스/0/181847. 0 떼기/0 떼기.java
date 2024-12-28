78dce2a051eda8d373ccafd05fdd2bc22789563d
GRIT <hinote444@naver.com>
2024-12-29 01:44:45 +0900

    [level 0] Title: 0 떼기, Time: 0.02 ms, Memory: 82.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181847. 0 떼기/0 떼기.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String solution(String n_str) {
+        int index = 0;
+        for (int i = 0; i < n_str.length(); i++) {
+            if (n_str.charAt(i) != '0') {
+                index = i;
+                break;
+            }
+        }
+        return n_str.substring(index);
+    }
+}
