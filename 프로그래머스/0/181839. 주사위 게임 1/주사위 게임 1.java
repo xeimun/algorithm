7cc5992c1ebba975b35f66841896c3afe806825a
GRIT <hinote444@naver.com>
2024-12-29 22:53:24 +0900

    [level 0] Title: 주사위 게임 1, Time: 0.09 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181839. 주사위 게임 1/주사위 게임 1.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int a, int b) {
+        boolean isOddA = a % 2 != 0;
+        boolean isOddB = b % 2 != 0;
+
+        if (isOddA && isOddB) {
+            return a * a + b * b;
+        }
+        if (isOddA || isOddB) {
+            return 2 * (a + b);
+        }
+        return Math.abs(a - b);
+    }
+}
