fd158a4722bac457efe4c52379c4919fc5500c21
GRIT <hinote444@naver.com>
2024-12-30 05:25:08 +0900

    [level 0] Title: l로 만들기, Time: 16.08 ms, Memory: 91.7 MB -BaekjoonHub

--- 프로그래머스/0/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/0/181834. l로 만들기/l로 만들기.java
@@ -1,11 +1,5 @@
 class Solution {
     public String solution(String myString) {
-        char[] arr = myString.toCharArray();
-        for (int i = 0; i < arr.length; i++) {
-            if(arr[i] < 108){
-                arr[i] = 'l';
-            }
-        }
-        return new String(arr);
+        return myString.replaceAll("[^l-z]", "l");
     }
 }
