d6584ffa26ace0e70ba8b23f2130d8c5a5a5a7be
GRIT <hinote444@naver.com>
2024-12-31 02:50:56 +0900

    [level 0] Title: 최빈값 구하기, Time: 0.40 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
+++ 프로그래머스/0/120812. 최빈값 구하기/최빈값 구하기.java
@@ -2,27 +2,20 @@ import java.util.*;
 import java.util.stream.*;
 class Solution {
     public int solution(int[] array) {
-        Map<Integer, Long> map = Arrays.stream(array)
-                                       .boxed()
-                                       .collect(Collectors.groupingBy(i -> i, Collectors.counting()));
-        long max = 0;
-        for (long l : map.values()) {
-            if (l > max) {
-                max = l;
+        int maxCount = 0;
+        int answer = 0;
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int number : array) {
+            int count = map.getOrDefault(number, 0) + 1;
+            map.put(number, count);
+            
+            if (count > maxCount) {
+                maxCount = count;
+                answer = number;
+            } else if (count == maxCount) {
+                answer = -1;
             }
         }
-
-        List<Integer> answer = new ArrayList<>();
-        for(Integer i : map.keySet()) {
-            if (map.get(i) == max) {
-                answer.add(i);
-            }
-        }
-        
-        if(answer.size() != 1){
-            return -1;
-        }
-
-        return answer.get(0);
+        return answer;
     }
 }
