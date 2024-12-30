5dc6baf0152bf12fb35bb45f9694d9d86dc996a3
GRIT <hinote444@naver.com>
2024-12-31 05:41:40 +0900

    [level 0] Title: 배열의 평균값, Time: 3.70 ms, Memory: 85.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Solution {
+    public double solution(int[] numbers) {
+        return Arrays.stream(numbers)
+                     .average()
+                     .orElse(0.0);
+    }
+}
